5bc1b1529df7ea844e80e570735036488c11eac0
amarees <saleh.amareen@dominos.com>
2024-07-17 17:47:31 -0400

    Added config properties to run GraphQL.

--- backend/src/main/java/com/ios/backend/BackendApplication.java
+++ backend/src/main/java/com/ios/backend/BackendApplication.java
@@ -2,8 +2,10 @@ package com.ios.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.annotation.PropertySource;
 
+@ServletComponentScan("graphql.resolvers")
 @SpringBootApplication
 @PropertySource("classpath:application.properties")
 public class BackendApplication {
